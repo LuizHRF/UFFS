c8800512b35a6ef789f4932172b97144b542b364
LuizHRF <106316500+LuizHRF@users.noreply.github.com>
2022-10-11 15:49:59 -0300

    Create 1010.c

---
+++ Estrutura de Dados/Lista 1/1010.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int c1, c2, q1, q2;
+    double p1, p2, total;
+    scanf("%d %d %lf %d %d %lf", &c1, &q1, &p1, &c2, &q2, &p2);
+    total =(q1*p1)+(q2*p2);
+    printf("VALOR A PAGAR: R$ %.2lf\n", total);
+    return 0;
+}
