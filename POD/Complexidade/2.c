0990e8b6b9bf2f80cdea19e5533848eeb6d61b6b
Luiz Faccio <106316500+LuizHRF@users.noreply.github.com>
2023-03-24 16:32:25 -0300

    Create 2.c

---
+++ POD/Complexidade/2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int v1, v2;
+    scanf("%d %d", &v1, &v2);
+    for(int i=1; i!=v2; i++){
+        v1+=v1;
+    }
+    printf("Valor final: %d\n", v1);
+}#include <stdio.h>
+
+int main(){
+    int v1, v2;
+    scanf("%d %d", &v1, &v2);
+    for(int i=1; i!=v2; i++){
+        v1+=v1;
+    }
+    printf("Valor final: %d\n", v1);
+}
