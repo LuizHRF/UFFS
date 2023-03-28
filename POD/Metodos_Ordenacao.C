495792238c7802c5e7f7926bba0ee290440ac714
Luiz Faccio <106316500+LuizHRF@users.noreply.github.com>
2023-03-28 20:51:53 -0300

    Update Metodos_Ordenacao.C

--- POD/Metodos_Ordenacao.C
+++ POD/Metodos_Ordenacao.C
@@ -1,6 +1,6 @@
 
 /**
- * @file Metodos_Ordenacao.c
+ * @file nome_do_arquivo.c
  * @author Luiz_Faccio
  * @version 0.1
  * @date 2023-03-28
@@ -53,8 +53,37 @@ int * bubbleSort(int *A, int size){
 
     return A;
 }
+
 // int * selectionSort(int *A, int size);
+int* selectionSort(int *A, int size){
+    int *min;
+    for(int i=0; i<size-1; i++){
+        min = &A[i];
+        for(int j=i+1; j<size-1; j++){
+            if(A[j]<*min){
+                min = &A[j];
+            }
+        }
+        troca(&A[i], min);
+    }
+    
+    return A;
+}
+
 // int * InsertionSort(int *A, int size);
+int* InsertionSort(int *A, int size){
+    for(int i=1; i<size;i++){
+        int *p = &A[i];
+        int j = i;
+        while(*p<A[j-1]&&j>0){
+            A[j] = A[j-1];
+            j--;
+        }
+        A[j] = *p;
+    }   
+    return A;
+}
+
 // int * mergeSort(int *A, int size);
 // int * quickSort(int *A, int size);
 // int * heapSort(int *A, int size);
@@ -71,15 +100,27 @@ int main(){
     printf("Vetor tamanho = %d\n", tamanhoVetor);
 
     // bubble sort
-    int *bubble = bubbleSort(vetor, tamanhoVetor);
-    printf("\nBubble sort: ");
-    for (i = 0 ; i < tamanhoVetor ; i++)
-        printf("%d ", bubble[i]);
-    printf("\n");
+    //int *bubble = bubbleSort(vetor, tamanhoVetor);
+    //printf("\nBubble sort: ");
+    //for (i = 0 ; i < tamanhoVetor ; i++)
+    //     printf("%d ", bubble[i]);
+    //printf("\n");
+    
+
 
     // selection sort
+    //int *selection = selectionSort(vetor, tamanhoVetor);
+    //printf("\nSelection sort: ");
+    //for (i = 0 ; i < tamanhoVetor ; i++)
+    //    printf("%d ", selection[i]);
+    //printf("\n");
 
     // insertion sort
+    int *insertion = InsertionSort(vetor, tamanhoVetor);
+    printf("\nSelection sort: ");
+    for (i = 0 ; i < tamanhoVetor ; i++)
+        printf("%d ", insertion[i]);
+    printf("\n");
 
     // merge sort
 
