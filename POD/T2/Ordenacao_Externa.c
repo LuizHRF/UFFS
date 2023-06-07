c691acceea85b24e92cfaf95b27f364bc8c1ed7a
Luiz Faccio <106316500+LuizHRF@users.noreply.github.com>
2023-06-06 21:27:18 -0300

    Update Ordenacao_Externa.c

--- POD/T2/Ordenacao_Externa.c
+++ POD/T2/Ordenacao_Externa.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//ORDENAÇÃO EXTERNA INCLOMPLETA 
 
 void insertionSort(int *A, int size){
     for(int i=1; i<size;i++){
@@ -20,23 +21,39 @@ int main()
 {
     FILE *fp = fopen("entrada.txt", "rw");
     
-    FILE *fteste[3] = {fopen("Mem1.txt", "w"), fopen("Mem2.txt", "w"), fopen("Mem3.txt", "w")};
+    FILE *fteste[3] = {fopen("Mem1.txt", "a"), fopen("Mem2.txt", "a"), fopen("Mem3.txt", "a")};
     
     int aux[5], i = 0;
+    int c = 0;
     
     while(!feof(fp)){
-        for(int j=0; j<5 && !feof(fp); j++){
-            fscanf(fp, "%d", &aux[j]);
+        c = 0;
+        
+        for(int j=0; j<5; j++){
+            fscanf(fp, "%d ", &aux[j]);
+            c++;
+            if(feof(fp)){break;}
         }
+        
         if(feof(fp)){break;}
+        
         insertionSort(aux, 5);
-        fprintf(fteste[i], "%d %d %d %d %d", aux[0], aux[1], aux[2], aux[3], aux[4]);
+        
+        fprintf(fteste[i], "%d %d %d %d %d ", aux[0], aux[1], aux[2], aux[3], aux[4]);
+        
         i++;
         if(i==3){i=0;}
     }
-    insertionSort(aux, i);
-    for(int m=0; m<i; m++){
-        fprintf(fteste[i], "%d", aux[m]);
+    printf("valor de c: %d\n", c);
+    for(int m=0; m<c; m++){
+        printf("valor de aux antes da ord: %d\n", aux[m]);
+    }
+    insertionSort(aux, c);
+    for(int m=0; m<c; m++){
+        printf("valor de aux depois da ord: %d\n", aux[m]);
+    }
+    for(int m=0; m<c; m++){
+        fprintf(fteste[i], " %d", aux[m]);
     }
     
     return 0;
