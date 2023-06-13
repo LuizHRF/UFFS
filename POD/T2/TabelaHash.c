ddaabaa4166d8de247870423d0c2274b9344f91c
Luiz Faccio <106316500+LuizHRF@users.noreply.github.com>
2023-06-13 13:42:25 -0300

    Update TabelaHash.c

--- POD/T2/TabelaHash.c
+++ POD/T2/TabelaHash.c
@@ -1,3 +1,27 @@
+/**
+ * @file TabelaHash.c
+ * @author Luiz_Faccio
+ * @version 0.1
+ * @date 2023-06-13
+ *
+ * @copyright Copyright (c) 2023
+ *
+ * @brief Arquivo template para Questão 02 do trabalho de Pesquisa e Ordenação T2.
+ *
+ *      Implemente uma tabela hash contendo 13 posições. O índice das chaves
+ *  deve ser gerado utilizando a fórmula k mod M, onde k é a chave e M a
+ *  quantidade de chaves. Resolva as colisões utilizando uma Lista Encadeada.
+ *  Inclua operações de inserção, remoção e busca da chave nesta tabela hash,
+ *  informando se a chave foi encontrada ou não.  Atenção: os elementos da
+ *  lista encadeada não precisam estar ordenados/em ordem.
+ *
+ *      Utilize os vetores comentados no código para realizar seus testes.
+ *
+ * Atenção: Antes de entregar, conferir se dos dados de documentação no início
+ * do arquivo estão preenchidos corretamente.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
