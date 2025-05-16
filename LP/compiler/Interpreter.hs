34f3f2f2a84e750e73b681ea674223410aba1bda
Luiz Faccio <106316500+LuizHRF@users.noreply.github.com>
2025-05-16 17:46:55 -0300

    Update Interpreter.hs

--- LP/compiler/Interpreter.hs
+++ LP/compiler/Interpreter.hs
@@ -8,28 +8,53 @@ isValue BFalse      = True
 isValue (Num _)     = True
 isValue _           = False
 
+subst :: String -> Expr -> Expr -> Expr
+subst v e (Var x) = if x==v then e else (Var x)
+subst v e (Lam n ex) = Lam n (subst v e ex)
+subst v e (App e1 e2) = App (subst v e e1) (subst v e e2)
+
+subst v e BTrue = BTrue
+subst v e BFalse = BFalse
+subst v e (Num x) = Num x
+subst v e (Add e1 e2) = Add (subst v e e1) (subst v e e2)
+subst v e (Sub e1 e2) = Sub (subst v e e1) (subst v e e2)
+subst v e (And e1 e2) = And (subst v e e1) (subst v e e2)
+subst v e (Or e1 e2) = Or (subst v e e1) (subst v e e2)
+subst v e (Mul e1 e2) = Mul (subst v e e1) (subst v e e2)
+subst v e (If e1 e2 e3) = If (subst v e e1) (subst v e e2) (subst v e e3)
+--not, Eq, Geq
+
+
 step :: Expr -> Expr 
 step (Add (Num n1) (Num n2)) = Num (n1 + n2)     -- S-Add
 step (Add (Num n1) e2) = let e2' = step e2       -- S-Add2
                            in Add (Num n1) e2' 
 step (Add e1 e2) = Add (step e1) e2              -- S-Add1
+
 step (And BTrue e2) = e2 
 step (And BFalse e2) = BFalse 
 step (And e1 e2) = And (step e1) e2 
+
 step (If BTrue e1 e2) = e1 
 step (If BFalse e1 e2) = e2
 step (If e e1 e2) = If (step e) e1 e2 
+
 step (Mul (Num n1) (Num n2)) = Num (n1*n2)
 step (Mul (Num n1) e) = Mul (Num n1) (step e)
 step (Mul e1 e2) = Mul (step e1) e2
+
 step (Sub (Num n) (Num n2)) = Num (n-n2)
+
 step (Or BTrue e1) = BTrue
 step (Or BFalse e1) = e1
+
 step (Not BTrue) = BFalse
 step (Not BFalse) = BTrue
+
 step (Eq (Num n1) (Num n2)) = if n1==n2 then BTrue else BFalse
 step (Eq (Num n1) e2) = Eq (Num n1) (step e2)
 step (Eq e1 e2) = Eq (step e1) e2
+
 step (Geq (Num n1) (Num n2)) = if n1>=n2 then BTrue else BFalse
 step (Geq (Num n1) e2) = Geq (Num n1) (step e2)
 step (Geq e1 e2) = Geq (step e1) e2
