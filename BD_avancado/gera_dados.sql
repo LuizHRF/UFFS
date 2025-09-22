51d2121263c8441863ff15cf4a1ca5784b383736
Luiz Faccio <luizhrigofaccio@outlook.com>
2025-09-22 10:44:38 -0300

    Geração de dados SQL

--- BD_avancado/gera_dados.sql
+++ BD_avancado/gera_dados.sql
@@ -2,24 +2,51 @@
 -- Atvidade sobre funções SQL
 -- Luiz Herique Rigo Faccio - 2211100003
 --==========================================
-CREATE SCHEMA faker;
-CREATE EXTENSION faker SCHEMA faker CASCADE;
 
-CREATE OR REPLACE FUNCTION gera_dados(n_usuarios int, n_carros int, n_funcionarios int, n_locacoes int)
-RETURNS void AS $$
+CREATE OR REPLACE PROCEDURE gera_dados(n_usuarios int, n_carros int, n_funcionarios int, n_locacoes int) AS $$
 BEGIN
 
-    PERFORM gera_usuarios(n_usuarios);
-    PERFORM gera_carros(n_carros);
-    PERFORM gera_funcionarios(n_funcionarios);
-    PERFORM gera_locacoes(n_locacoes);
+    CALL gera_usuarios(n_usuarios);
+    CALL gera_carros(n_carros);
+    CALL gera_funcionarios(n_funcionarios);
+    CALL gera_locacoes(n_locacoes);
 
-    RETURN;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION gera_usuarios(n_usuarios int)
-RETURNS void AS $$
+CREATE OR REPLACE PROCEDURE insere_dados_iniciais() AS $$
+BEGIN
+
+    INSERT INTO Tipos_Veiculos(codTipo, descricao) VALUES (1, 'Veleiro'), 
+                                                (2, 'Jet-Ski'),
+                                                (3, 'Catamaran'), 
+                                                (4, 'Iate');
+
+    INSERT INTO Habilitacoes (tipo, idade_min, descricao) VALUES 
+    ('Veleiro',        8,  'Veleiro'),
+    ('Motonauta',      18, 'Jet-Ski'),
+    ('Arrais-Amador',  18, 'Pode dirigir todas as embarcações exceto Jet-Ski'),
+    ('Mestre-Amador',  18, 'Pode dirigir todas as embarcações'),
+    ('Capitão-Amador', 18, 'Pode dirigir todas as embarcações');
+
+    INSERT INTO Veiculos_Habilitacoes (codTipo, codH) values 
+		
+												(1, 1),
+												(1, 3),
+												(1, 4),
+												(1, 5),
+												(2, 2),
+												(3, 3),
+												(3, 4),
+												(3, 5),
+												(4, 3),
+												(4, 4),
+												(4, 5);
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE gera_usuarios(n_usuarios int) AS $$
 DECLARE
     _CPF VARCHAR(11);
     _nome VARCHAR(30);
@@ -35,25 +62,23 @@ BEGIN
     FOR i IN 1..n_usuarios LOOP
 
         _CPF := lpad((trunc(random() * 99999999999))::text, 11, '0');
-        _nome := faker.name();
-        _endereco := faker.address();
+        _nome := 'Cliente_' || i;
+        _endereco := 'Endereço_' || i;
         _estado_civil := estados_civis[trunc(random() * array_length(estados_civis, 1) + 1)];
         _num_filhos := trunc(random() * 5);
-        _data_nasc := faker.date_between('1960-01-01', '2006-12-31');
-        _telefone := faker.phone_number();   
+        _data_nasc := (date '1960-01-01' + (random() * (date '2006-12-31' - date '1960-01-01'))::int);
+        _telefone := '(' || 10 + (trunc(random() * 89))::INT || ') ' || lpad((trunc(random() * 99999999))::text, 8, '0');
 
         SELECT codH INTO _codH FROM Habilitacoes ORDER BY random() LIMIT 1;
 
         INSERT INTO Clientes (CPF, nome, endereco, estado_civil, num_filhos, data_nasc, telefone, codH) 
         VALUES (_CPF, _nome, _endereco, _estado_civil, _num_filhos, _data_nasc, _telefone, _codH);
     END LOOP;
-    RETURN;
     
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION gera_carros(n_carros int)
-RETURNS void AS $$
+CREATE OR REPLACE PROCEDURE gera_carros(n_carros int) AS $$
 DECLARE
     _matricula INT;     
     _nome VARCHAR(30);
@@ -62,13 +87,15 @@ DECLARE
     _potMotor FLOAT;
     _vlDiaria FLOAT;
     _codTipo INT;  --(Ler codtipos, escolher aleatório)
+    modelos VARCHAR[] := ARRAY[ 'Cutter', 'Flash 165', 'Fluvimar BR 6000', 'Sea Doo GTI 130', 'Yamaha VX 1100 Deluxe', 'VX de Luxe 2021', 'VX Cruiser 2021', 'FX SHO 2021', 'Lavezzi', 'Lipari 41', 'Ocema 42', 'Levefort 40P', 'Prestige 500', 'C38', 
+                                'Prestige 500', 'Azimut 740', 'Prestige 440s', 'Prestige 500',  'C38', 'Prestige 500', 'Azimut 740', 'Prestige 440s', 'Open590'];
 BEGIN
 
     FOR i IN 1..n_carros LOOP
 
-        _matricula := trunc(random() * 9999);
-        _nome := faker.vehicle_make();
-        _modelo := faker.vehicle_model();
+        _matricula := i;
+        _nome := 'Barco_' || trunc(random() * 9999)::INT;
+        _modelo := modelos[trunc(random() * array_length(modelos, 1) + 1)];
         _comprimento := round((random() * 30 + 5)::numeric, 2);
         _potMotor := round((random() * 2000 + 100)::numeric, 2);
         _vlDiaria := round((random() * 7000 + 1000)::numeric, 1);
@@ -78,13 +105,11 @@ BEGIN
         INSERT INTO Veiculos (matricula, nome, modelo, comprimento, potMotor, vlDiaria, codTipo) 
         VALUES (_matricula, _nome, _modelo, _comprimento, _potMotor, _vlDiaria, _codTipo);
     END LOOP;
-    
-    RETURN;
+
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION gera_funcionarios(n_funcionarios int)
-RETURNS void AS $$
+CREATE OR REPLACE PROCEDURE gera_funcionarios(n_funcionarios int) AS $$
 DECLARE
     _nome VARCHAR(30);
     _telefone VARCHAR(15);
@@ -94,23 +119,20 @@ DECLARE
 BEGIN
 
     FOR i IN 1..n_funcionarios LOOP
-
-        _nome := faker.name();
-        _telefone := faker.phone_number();
-        _endereco := faker.address();
-        _idade := trunc(random() * 40 + 18);
-        _salario := round((random() * 5000 + 2000)::numeric, 2);
+    _nome := 'Funcionario_' || i;
+    _telefone := '(' || (10 + trunc(random() * 89))::INT || ') ' || lpad((trunc(random() * 99999999))::text, 8, '0');
+    _endereco := 'Endereco_' || i;
+    _idade := trunc(random() * 40 + 18);
+    _salario := round((random() * 5000 + 2000)::numeric, 2);
 
         INSERT INTO Funcionarios (nome, telefone, endereco, idade, salario) 
         VALUES (_nome, _telefone, _endereco, _idade, _salario);
     END LOOP;
 
-    RETURN;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION gera_locacoes(n_locacoes int)
-RETURNS void AS $$
+CREATE OR REPLACE PROCEDURE gera_locacoes(n_locacoes int) AS $$
 DECLARE
     _valor FLOAT;
     _inicio DATE;
@@ -124,7 +146,7 @@ BEGIN
 
     FOR i IN 1..n_locacoes LOOP
 
-        _inicio := faker.date_between('2021-01-01', '2024-12-31');
+        _inicio := (date '2022-01-01' + (random() * (date '2022-12-31' - date '2022-01-01'))::int);
         days_diff := trunc(random() * 15 + 1);
         _fim := _inicio + days_diff;
         _obs := 'Locação de ' || days_diff || ' dias';
@@ -138,7 +160,6 @@ BEGIN
         VALUES (_valor, _inicio, _fim, _obs, _matricula, _codF, _CPF);
     END LOOP;
 
-    RETURN;
 END;
 $$ LANGUAGE plpgsql;
 
